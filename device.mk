fb3c95eaf819bce521cbcef8c34f4730e6cd5dc5
jenkins <jenkins@teamw.in>
2015-03-14 18:44:52 -0400

    trying to implement hw keystore

--- device.mk
+++ device.mk
@@ -14,6 +14,9 @@ else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
+$(shell mkdir -p out/target/product/hlte/recovery/root/vendor/firmware/keymaster)
+$(shell mkdir -p out/target/product/hlte/recovery/root/vendor/lib/hw)
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
@@ -21,11 +24,20 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/fstab.qcom:recovery/root/fstab.qcom
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/libkeyutils.so:recovery/root/sbin/libkeyutils.so \
-    $(LOCAL_PATH)/recovery/libsec_ecryptfs.so:recovery/root/sbin/libsec_ecryptfs.so \
-    $(LOCAL_PATH)/recovery/libsec_km.so:recovery/root/sbin/libsec_km.so \
     $(LOCAL_PATH)/recovery/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
-	$(LOCAL_PATH)/selinux/file_contexts:recovery/root/prebuilt_file_contexts
+    $(LOCAL_PATH)/selinux/file_contexts:recovery/root/prebuilt_file_contexts \
+    $(LOCAL_PATH)/recovery/keymaster.b00:recovery/root/vendor/firmware/keymaster/keymaster.b00 \
+    $(LOCAL_PATH)/recovery/keymaster.b01:recovery/root/vendor/firmware/keymaster/keymaster.b01 \
+    $(LOCAL_PATH)/recovery/keymaster.b02:recovery/root/vendor/firmware/keymaster/keymaster.b02 \
+    $(LOCAL_PATH)/recovery/keymaster.b03:recovery/root/vendor/firmware/keymaster/keymaster.b03 \
+    $(LOCAL_PATH)/recovery/keymaster.mdt:recovery/root/vendor/firmware/keymaster/keymaster.mdt \
+    $(LOCAL_PATH)/recovery/cmnlib.b00:recovery/root/vendor/firmware/cmnlib.b00 \
+    $(LOCAL_PATH)/recovery/cmnlib.b01:recovery/root/vendor/firmware/cmnlib.b01 \
+    $(LOCAL_PATH)/recovery/cmnlib.b02:recovery/root/vendor/firmware/cmnlib.b02 \
+    $(LOCAL_PATH)/recovery/cmnlib.b03:recovery/root/vendor/firmware/cmnlib.b03 \
+    $(LOCAL_PATH)/recovery/cmnlib.mdt:recovery/root/vendor/firmware/cmnlib.mdt \
+    $(LOCAL_PATH)/recovery/libQSEEComAPI.so:recovery/root/vendor/lib/libQSEEComAPI.so
+    $(LOCAL_PATH)/recovery/keystore.msm8974.so:recovery/root/vendor/lib/hw/keystore.msm8974.so
 
 
 $(call inherit-product, build/target/product/full.mk)

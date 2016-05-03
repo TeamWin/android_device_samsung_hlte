4c195995251fbedb3894dc55292f38da28badbb9
James Christopher Adduono <jc@adduono.com>
2016-05-03 18:41:48 -0400

    New kernel, enable AOSP/CM13 encryption (1.0), cleanups
    Previous kernel had too many hacks for OC, too new drivers
    resulted in touchkey firmware updates that broke
    touchkeys in older firmware.
    
    Remove SuperSU as well, it's just not worth the trouble.
    
    Also, they're all hlte now. Okay. Stop complaining!
    
    Change-Id: I2180a699b8a1d6bd2d3688718e5bb82bdf13ad4b

--- bootimg.mk
+++ bootimg.mk
@@ -2,21 +2,28 @@ MKBOOTIMG := device/samsung/hlte/mkbootimg
 
 FLASH_IMAGE_TARGET ?= $(PRODUCT_OUT)/recovery.tar
 
+BUILT_RAMDISK_CPIO := $(PRODUCT_OUT)/ramdisk-recovery.cpio
+COMPRESS_COMMAND := xz --format=lzma --lzma1=dict=16MiB
+
 ifdef TARGET_PREBUILT_DTB
 	BOARD_MKBOOTIMG_ARGS += --dt $(TARGET_PREBUILT_DTB)
 endif
 
 INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(recovery_ramdisk)
+	@echo "------- Compressing recovery ramdisk -------"
+	$(hide) $(COMPRESS_COMMAND) "$(BUILT_RAMDISK_CPIO)"
 	@echo "------- Making recovery image -------"
 	$(hide) $(MKBOOTIMG) \
 		--kernel $(TARGET_PREBUILT_KERNEL) \
-		--ramdisk $(PRODUCT_OUT)/ramdisk-recovery.img \
+		--ramdisk $(BUILT_RAMDISK_CPIO).lzma \
 		--cmdline "$(BOARD_KERNEL_CMDLINE)" \
 		--base $(BOARD_KERNEL_BASE) \
 		--pagesize $(BOARD_KERNEL_PAGESIZE) \
 		$(BOARD_MKBOOTIMG_ARGS) \
 		-o $(INSTALLED_RECOVERYIMAGE_TARGET)
 	@echo "------- Made recovery image: $@ -------"
+	$(hide) echo -n "SEANDROIDENFORCE" >> $(INSTALLED_RECOVERYIMAGE_TARGET)
+	@echo "------- Lied about SEAndroid state to Samsung bootloader -------"
 	$(hide) tar -C $(PRODUCT_OUT) -H ustar -c recovery.img > $(FLASH_IMAGE_TARGET)
 	@echo "------- Made flashable image: $(FLASH_IMAGE_TARGET) -------"

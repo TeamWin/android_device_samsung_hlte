0907eb05d781864998e2fa12c31931978ab39f3f
ripee <ripee.dev@gmail.com>
2018-02-04 18:44:59 -0500

    Update omni_hlte.mk

--- omni_hlte.mk
+++ omni_hlte.mk
@@ -1,10 +1,15 @@
 # Release name
 PRODUCT_RELEASE_NAME := hlte
 
-$(call inherit-product, build/target/product/embedded.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
+
+# Time Zone data for Recovery
+PRODUCT_COPY_FILES += \
+    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hlte

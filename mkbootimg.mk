b17e16cddf4644aa74f23c65f23c5daafee188a7
ripee <ripee.dev@gmail.com>
2018-02-04 19:42:21 -0500

    Update mkbootimg.mk

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -1,11 +1,27 @@
-BUILT_RAMDISK_CPIO := $(PRODUCT_OUT)/ramdisk-recovery.cpio
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+
+BUILT_RAMDISK_CPIO := $(LOCAL_PATH)/ramdisk-recovery.cpio
 COMPRESS_COMMAND := xz --format=lzma --lzma1=dict=16MiB
 
 ifdef TARGET_PREBUILT_DTB
 	BOARD_MKBOOTIMG_ARGS += --dt $(TARGET_PREBUILT_DTB)
 endif
 
-INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
+INSTALLED_RECOVERYIMAGE_TARGET := $(LOCAL_PATH)/recovery.img
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(recovery_ramdisk)
 	@echo "------- Compressing recovery ramdisk -------"
 	$(hide) $(COMPRESS_COMMAND) "$(BUILT_RAMDISK_CPIO)"
